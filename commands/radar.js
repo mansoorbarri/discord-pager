ccf23f3617177e76e54a00795a577c8dc367fd8e
Mansoor Barri <mansoor.eb.ak@gmail.com>
2026-04-16 18:47:02 +0000

    Update radar help userscript links

--- commands/radar.js
+++ commands/radar.js
@@ -15,9 +15,10 @@ To use the radar addon, please follow these steps:
    - Toggle on **Developer mode** in the top right corner
    - Click on Tampermonkey to expand its details
    - Enable **"Allow user scripts"** (this is required for Tampermonkey to work)
-3. Make sure you have installed the radar addon script: <https://xyzmani.com/radar>
-4. After installing the script, go to GeoFS and ensure you have entered your departure, arrival, and callsign, then clicked "Save".
-5. Now, you should be able to see yourself at: https://radarthing.com
+3. If you're using Tampermonkey, install the radar addon script here: <https://radarthing.com/userscript>
+4. You can also copy-paste the code manually from <https://radarthing.com/loader>, but that is still not recommended.
+5. After installing the script, go to GeoFS and ensure you have entered your departure, arrival, and callsign, then clicked "Save".
+6. Now, you should be able to see yourself at: https://radarthing.com
     `;
   await interaction.reply({ content: message });
 }
