329f4d9dab6de790eac0a8413780dc81729d41b0
Mansoor Barri <mansoor.eb.ak@gmail.com>
2026-04-17 00:07:02 +0000

    Revise radar help response

--- commands/radar.js
+++ commands/radar.js
@@ -7,18 +7,24 @@ export const data = new SlashCommandBuilder()
 
 export async function execute(interaction) {
   const message = `
-To use the radar addon, please follow these steps:
-
+If you're using Tampermonkey:
 1. Make sure you have Tampermonkey installed: <https://www.tampermonkey.net/>
 2. Enable Developer Mode and allow user scripts:
-   - Go to your browser's extensions page (e.g., \`chrome://extensions\`)
-   - Toggle on **Developer mode** in the top right corner
-   - Click on Tampermonkey to expand its details
-   - Enable **"Allow user scripts"** (this is required for Tampermonkey to work)
-3. If you're using Tampermonkey, install the radar addon script here: <https://radarthing.com/userscript>
-4. You can also copy-paste the code manually from <https://radarthing.com/loader>, but that is still not recommended.
-5. After installing the script, go to GeoFS and ensure you have entered your departure, arrival, and callsign, then clicked "Save".
-6. Now, you should be able to see yourself at: https://radarthing.com
+   * Go to your browser's extensions page (e.g., \`chrome://extensions\`)
+   * Toggle on Developer mode in the top right corner
+   * Click on Tampermonkey to expand its details
+   * Enable "Allow user scripts" (this is required for Tampermonkey to work)
+3. Install the radar addon script here: <https://radarthing.com/userscript>
+
+If you aren't using Tampermonkey (not recommended)
+1. Copy-paste the code manually from <https://radarthing.com/loader>
+2. Open GeoFS, right click on the page, and choose inspect or inspect element
+3. Navigate to the Console tab and paste the script in
+
+------------------------------------------------------------
+
+4. After installing the script, go to GeoFS and ensure you have entered your departure, arrival, and callsign, then clicked "Save".
+5. Now, you should be able to see yourself at: <https://radarthing.com>
     `;
   await interaction.reply({ content: message });
 }
